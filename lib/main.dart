15de116714354d96bdc002ec3337c95ab4e4a86d
HyunWoo <iu4148@gmail.com>
2023-09-03 03:24:42 +0900

    Version 1.0.0.0903.1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import '../screens/home_page.dart';
 import '../screens/loading_page.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 
 void main() async {
   await initializeDateFormatting();
--- lib/screens/loading_page.dart
+++ lib/screens/loading_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:period_calendar/screens/First_add_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'home_page.dart';
 
 class LoadingPage extends StatefulWidget {
   @override
@@ -9,9 +12,64 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPage extends State<LoadingPage> {
+  DateTime date = DateTime.now();
+  void checkData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final bool? check = prefs.getBool('check');
+    if (check != null) {
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+          builder: (BuildContext context) =>
+              HomePage()), (route) => false);
+    }
+    else {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              '데이터가 필요합니다.',
+              style: TextStyle(fontSize: 16,),
+            ),
+            content: SingleChildScrollView(child:new Text("가장 최근 생리일을 입력해주세요.")),
+            actions: <Widget>[
+              new TextButton(
+                child: new Text("확인"),
+                onPressed: () async {
+                  final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: date,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime.now(),
+                    initialEntryMode: DatePickerEntryMode.calendarOnly,
+                  );
+                  if (selectedDate != null) {
+                    setState(() {
+                      date = selectedDate;
+                    });
+                  }
+                  if (selectedDate == null) {
+                    exit(0);
+                  }
+                  else {
+                    prefs.setBool('check', true);
+                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            HomePage()), (route) => false);
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
   @override
   void initState() {
     super.initState();
+    sleep(Duration(seconds: 2));
+    checkData();
   }
   @override
   Widget build(BuildContext context) {
@@ -27,11 +85,6 @@ class _LoadingPage extends State<LoadingPage> {
                 fontFamily: 'title',
                 fontSize: 32,
               ),
-            ),
-            SizedBox(height: 15,),
-            SpinKitFadingCircle(
-              color: Colors.pinkAccent,
-              size: 64.0,
             ),
           ],
         ),
